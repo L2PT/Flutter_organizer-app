ff1a2687c9151bbe2d404be18bfab1946617dc59
turrini97lorenzo@gmail.com <Turrini97>
2020-09-24 23:24:32 +0200

    Correzione bug persistent notification, notifiche fatte ma da TESTARE

--- lib/plugins/firebase/firebase_messaging.dart
+++ lib/plugins/firebase/firebase_messaging.dart
@@ -4,16 +4,12 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:venturiautospurghi/bloc/mobile_bloc/mobile_bloc.dart';
 import 'package:venturiautospurghi/models/account.dart';
 import 'package:venturiautospurghi/plugins/dispatcher/platform_loader.dart';
 import 'package:venturiautospurghi/repositories/cloud_firestore_service.dart';
 import 'package:venturiautospurghi/utils/global_constants.dart';
 import 'package:venturiautospurghi/models/event.dart';
-import 'package:venturiautospurghi/utils/global_methods.dart';
-import 'package:venturiautospurghi/utils/extensions.dart';
-import 'package:venturiautospurghi/utils/theme.dart';
 import 'package:http/http.dart' as http;
 
 class FirebaseMessagingService {
@@ -30,21 +26,22 @@ class FirebaseMessagingService {
     if (Platform.isIOS) requestiOSPermission();
     //check if token is up to date
     String token = await _firebaseMessaging.getToken();
-    if(token != account.token) {
+    if (token != account.token) {
       _databaseRepository.updateAccountField(account.id, "Token", token);
-      if(Constants.debug) print("New token: " + token);
+      if (Constants.debug) print("New token: " + token);
     }
-    //configure behaviour handlers TODO
-//    _firebaseMessaging.configure(
-//      onMessage: onMessageHandler,
-//      onResume: onResumeHandler,
-//      onLaunch: onLaunchHandler,
-//      onBackgroundMessage: onBackgroundMessageHandler,
-//    );
+
+    _firebaseMessaging.configure(
+      onMessage: onMessageHandler,
+      onResume: onResumeHandler,
+      onLaunch: onLaunchHandler,
+      onBackgroundMessage: onBackgroundMessageHandler,
+    );
+
   }
 
   Future<dynamic> onMessageHandler(Map<String, dynamic> message) async {
-    if(Constants.debug) print('on message: $message');
+    if (Constants.debug) print('on message: $message');
     if (_isFeedbackNotification(message)) {
       PlatformUtils.notifyInfoMessage(message["notification"]["title"]);
     } else {
@@ -53,57 +50,75 @@ class FirebaseMessagingService {
   }
 
   Future<dynamic> onResumeHandler(Map<String, dynamic> message) async {
-    if(Constants.debug) print('on resume: $message');
+    if (Constants.debug) print('on resume: $message');
     _openTheEvent(message);
   }
 
   Future<dynamic> onLaunchHandler(Map<String, dynamic> message) async {
-    if(Constants.debug) print('on launch: $message');
+    if (Constants.debug) print('on launch: $message');
     _openTheEvent(message);
   }
 
   static Future<dynamic> onBackgroundMessageHandler(Map<String, dynamic> message) async {
-    if(Constants.debug) print('on background message: $message');//TODO
-//    if (!_isFeedbackNotification(message))
-//      _updateEventAndSendFeedback(message, Status.Delivered);
+    if (Constants.debug) print('on background message: $message');
+    if (message.containsKey('data')) {
+      // Handle data message
+      final dynamic data = message['data'];
+      if(message['data']['id'].isNullOrEmpty()){
+        CloudFirestoreService databaseRepository = await CloudFirestoreService.create();
+        databaseRepository.updateEventField(message['data']['id'], "Stato", Status.Delivered);
+        Event event = await databaseRepository.getEvent(message['data']['id']);
+        Account supervisor = event.operator
+          ..id = event.supervisor.id; //get here to prevent old token
+        sendNotification(
+            token: supervisor.token,
+            title: "L'avviso è stato cosegnato a ${(event.operator as Account)
+                .surname} ${(event.operator as Account).name}");
+      }
+    }
   }
 
   void requestiOSPermission() {
-    _firebaseMessaging.requestNotificationPermissions(IosNotificationSettings(sound: true, badge: true, alert: true));
-    _firebaseMessaging.onIosSettingsRegistered.listen((IosNotificationSettings settings) {
+    _firebaseMessaging.requestNotificationPermissions(
+        IosNotificationSettings(sound: true, badge: true, alert: true));
+    _firebaseMessaging.onIosSettingsRegistered.listen((
+        IosNotificationSettings settings) {
       print("Settings registered: $settings");
     });
   }
 
-  bool _isFeedbackNotification(Map<String, dynamic> message){
+  bool _isFeedbackNotification(Map<String, dynamic> message) {
     return message['data']['id'].isNullOrEmpty();
   }
 
-  Future<Event> _updateEventAndSendFeedback(Map<String, dynamic> message, int newStatus) async {
+  Future<Event> _updateEventAndSendFeedback(Map<String, dynamic> message,
+      int newStatus) async {
     _databaseRepository.updateEventField(message['data']['id'], "Stato", newStatus);
-    _databaseRepository.getEvent(message['data']['id']);
     Event event = await _databaseRepository.getEvent(message['data']['id']);
-    Account supervisor = event.operator..id = event.supervisor.id;//get here to prevent old token
+    Account supervisor = event.operator
+      ..id = event.supervisor.id; //get here to prevent old token
     sendNotification(
         token: supervisor.token,
-        title: "L'avviso è stato cosegnato a ${(event.operator as Account).surname} ${(event.operator as Account).name}");
+        title: "L'avviso è stato cosegnato a ${(event.operator as Account)
+            .surname} ${(event.operator as Account).name}");
     return event;
   }
 
   //TODO to test the expected behaviour is the app opens in notification status and automatically to the event
   void _openTheEvent(Map<String, dynamic> message) async {
     if (_isFeedbackNotification(message) && account.supervisor) {
-      context.bloc<MobileBloc>().add(NavigateEvent(Constants.waitingEventListRoute, null));
+      context.bloc<MobileBloc>().add(
+          NavigateEvent(Constants.waitingEventListRoute, null));
     } else {
       Event event = await _databaseRepository.getEvent(message['data']['id']);
       PlatformUtils.navigator(context, event);
     }
   }
 
-  static void sendNotification( {token = "",
-      title = "Nuovo incarico assegnato",
-      description = "Clicca la notifica per vedere i dettagli",
-      eventId = ""}) async {
+  static void sendNotification({token = "",
+    title = "Nuovo incarico assegnato",
+    description = "Clicca la notifica per vedere i dettagli",
+    eventId = ""}) async {
     String url = "https://fcm.googleapis.com/fcm/send";
     String json = "";
     Map<String, String> notification = new Map<String, String>();
@@ -113,15 +128,19 @@ class FirebaseMessagingService {
     notification['body'] = description;
     notification['click_action'] = "FLUTTER_NOTIFICATION_CLICK";
     data['id'] = eventId;
-    json += "\"notification\":" + jsonEncode(notification) + ", \"data\":" + jsonEncode(data) + "}";
+    json += "\"notification\":" + jsonEncode(notification) + ", \"data\":" +
+        jsonEncode(data) + "}";
     var response = await http.post(
         url,
         body: json,
-        headers: {"Authorization": "key=AIzaSyBF13XNJM1LDuRrLcWdQQxuEcZ5TakypEk",
-          "Content-Type": "application/json"},
+        headers: {
+          "Authorization": "key=AIzaSyBF13XNJM1LDuRrLcWdQQxuEcZ5TakypEk",
+          "Content-Type": "application/json"
+        },
         encoding: Encoding.getByName('utf-8'));
     print("response: " + jsonEncode(json));
     print("response: " + response.body);
   }
+
 
 }
